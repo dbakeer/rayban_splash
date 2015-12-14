11ab0e8660114eb7e199a65039c5c2235f04a62b
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-12-14 14:36:16 -0500

    just numeric keypad and password working

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-jQuery('[name=form1]').submit(function(evt){
-  var numberVal = jQuery(evt.target).find('[name=number1]').val();
-  if(numberVal != "1") {
-    evt.preventDefault();
+// numeric keypad
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
   }
+}
+
+// keypad
+$( document ).ready(function() {
+
+ $(".pass").keydown( function() {
+ pass=$("#digit1").val() + $("#digit2").val() + $("#digit3").val() + $("#digit4").val();
+
+ correctPass = "1937";
+    if(pass==correctPass)    {
+         $("#submit").removeAttr("disabled");
+    }
+    else    {
+         $("#submit").attr('disabled','disabled');
+    }
+   });
 });
