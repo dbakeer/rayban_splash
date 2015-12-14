1f1d7f01bc09febea23c9e02ecdfe8033f783935
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-12-14 16:58:10 -0500

    consolidated to one index

--- app.js
+++ app.js
@@ -2,26 +2,35 @@
 function validate(evt) {
   var theEvent = evt || window.event;
   var key = theEvent.keyCode || theEvent.which;
-  key = String.fromCharCode( key );
+  key = String.fromCharCode(key);
   var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
+  if (!regex.test(key)) {
     theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
+    if (theEvent.preventDefault)
+    theEvent.preventDefault();
   }
 }
 
 // keypad
-$( document ).ready(function() {
+$(document).ready(function() {
+  $("#content").hide();
 
- $(".pass").keydown( function() {
- pass=$("#digit1").val() + $("#digit2").val() + $("#digit3").val() + $("#digit4").val();
+  $(".pass").keydown( function() {
+    pass =
+    $("#digit1").val() +
+    $("#digit2").val() +
+    $("#digit3").val() +
+    $("#digit4").val();
 
- correctPass = "1937";
-    if(pass==correctPass)    {
-         $("#submit").removeAttr("disabled");
+    correctPass = "1937";
+    if(pass==correctPass) {
+      $("#keycode").fadeOut(1000);
+      $("#content").fadeIn(1000);
     }
-    else    {
-         $("#submit").attr('disabled','disabled');
+    else {
+      // $("#keycode").effect("shake");
+      // $("#message").html('***Invalid email or password***');
+      console.log("Incorrect password");
     }
-   });
+  });
 });
