ba04935435dc568dfe7f16b72c22d2e0b8568b4a
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-12-14 20:23:15 -0500

    RSVP text complete

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ function validate(evt) {
 
 // keypad
 $(document).ready(function() {
-  $("#content").hide();
+  $("#logo").hide();
+  $("#invite").hide();
 
   $(".pass").keydown(function(){
-    pass = $("#digit1").val() + $("#digit2").val() + $("#digit3").val() + $("#digit4").val();
+    pass = $("#digit1").val() +
+           $("#digit2").val() +
+           $("#digit3").val() +
+           $("#digit4").val();
 
     correctPass = "1937";
 
@@ -24,7 +28,11 @@ $(document).ready(function() {
       if (pass==correctPass) {
         $("#keypad").fadeOut(1000, function(){
           $(this).remove();
-          $("#content").fadeIn(8000);
+          $("#logo").fadeIn(8000);
+          $("#logo").fadeOut(2000, function(){
+            $(this).remove();
+            $("#invite").fadeIn(5000);
+          });
         });
       } else {
         $("#keycode").effect("shake");
