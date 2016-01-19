101e95ad40f31a72f9396fcbae5fcf6e007a06a3
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-01-19 10:34:54 -0500

    cleaning up the code for presentation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,27 @@
-// numeric keypad
-// function validate(evt) {
-//   var theEvent = evt || window.event;
-//   var key = theEvent.keyCode || theEvent.which;
-//   key = String.fromCharCode(key);
-//   var regex = /[0-9]|\./;
-//   if (!regex.test(key)) {
-//     theEvent.returnValue = false;
-//     if (theEvent.preventDefault)
-//     theEvent.preventDefault();
-//   }
-// }
-
-
 // keypad
 $(document).ready(function() {
 
-  $("#logo").hide();
+  // hide the video div until correct keycode is entered
   $("#video").hide();
-  $("#actualvideo").get(0).pause();
-  $("#invite").hide();
-  $("#rsvpmessage").hide();
-  $(".icon-container").hide();
-  $('#mobilewatch').hide();
-  $("#digit1").focus();
-  $('form > input').keyup(function() {
-    var empty1 = false;
-    $('form > input').each(function() {
-      if ($(this).val() === '') {
-        empty1 = true;
-      }
-    });
-    if (empty1) {
-      $('#formsubmit').attr('disabled', 'disabled');
-    } else {
-      $('#formsubmit').prop("disabled", false);
-    }
-  });
 
+  // pause the video until start
+  $("#actualvideo").get(0).pause();
+
+  // hide the invite details
+  $("#invite").hide();
+
+  // hide the RSVP form
+  $("#rsvpmessage").hide();
+
+  // hide mobile alternate view
+  $('#mobilewatch').hide();
+
+  // automatically tab to the first keycode box
+  $("#val1").focus();
+
+  ///////////////////////
+  /////// KEYCODE ///////
+  ///////////////////////
   $(".pass").keyup(function(){
 
     // autotab function
@@ -49,10 +34,10 @@ $(document).ready(function() {
     break;
   }
 
-    pass = $("#digit1").val() +
-           $("#digit2").val() +
-           $("#digit3").val() +
-           $("#digit4").val();
+    pass = $("#val1").val() +
+           $("#val2").val() +
+           $("#val3").val() +
+           $("#val4").val();
 
     correctPass = "SOHO";
     correctPass2 = "soho";
@@ -92,11 +77,11 @@ $(document).ready(function() {
         $("#keycode").effect("shake");
         $("#message").html('INCORRECT PASSWORD');
         $('#hint').html('HINT : THE LOCATION OF THE NEW RAY-BAN STORE');
-        $("#digit1").val('');
-        $("#digit2").val('');
-        $("#digit3").val('');
-        $("#digit4").val('');
-        $("#digit1").focus();
+        $("#val1").val('');
+        $("#val2").val('');
+        $("#val3").val('');
+        $("#val4").val('');
+        $("#val1").focus();
       }
     }
   });
@@ -105,6 +90,25 @@ $(document).ready(function() {
   $("#rsvp").click(function(){
     $("#invite").remove();
     $("#rsvpmessage").fadeIn(2000);
+  });
+
+  ///////////////////////
+  ////// RSVP FORM //////
+  ///////////////////////
+  $('form > input').keyup(function() {
+    // if form is empty, do not permit the user to submit it
+    var empty1 = false;
+    $('form > input').each(function() {
+      if ($(this).val() === '') {
+        empty1 = true;
+      }
+    });
+    if (empty1) {
+      $('#formsubmit').attr('disabled', 'disabled');
+    } else {
+      // disable the disabling of the button and allow form submission
+      $('#formsubmit').prop("disabled", false);
+    }
   });
 
   $("#rewatch").click(function(){
